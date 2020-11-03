fe74cccda96fda9bbb35c941010a3c9a79b8e840
ououopel0001 <71679877+ououopel0001@users.noreply.github.com>
2020-11-03 11:59:06 +0800

    控制器

--- manycard1/Assets/script/gamemanager.cs
+++ manycard1/Assets/script/gamemanager.cs
@@ -2,6 +2,26 @@
 
 public class gamemanager : MonoBehaviour
 {
+    [Header("slime")]
     public Transform slime;
+    [Header("TurtleShell")]
     public Transform TurtleShell;
+    [Header("虛擬搖桿")]
+    public FixedJoystick joystick;
+    [Header("旋轉速度"),Range(0.1f, 20f)]
+    public float turn = 1.5f;
+
+    private void Start()
+    {
+        print("開始事件");
+    }
+
+    private void Update()
+    {
+        print("更新事件");
+        print(joystick.Vertical);
+
+        slime.Rotate(0, joystick.Horizontal, 0);
+        TurtleShell.Rotate(0, joystick.Horizontal * turn , 0);
+    }
 }
