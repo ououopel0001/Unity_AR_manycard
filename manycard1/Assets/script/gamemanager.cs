c04e5ab5e733a036c7aa4730c616855e841462bc
ououopel0001 <71679877+ououopel0001@users.noreply.github.com>
2020-11-10 12:01:07 +0800

    完成與發佈

--- manycard1/Assets/script/gamemanager.cs
+++ manycard1/Assets/script/gamemanager.cs
@@ -10,6 +10,11 @@ public class gamemanager : MonoBehaviour
     public FixedJoystick joystick;
     [Header("旋轉速度"),Range(0.1f, 20f)]
     public float turn = 1.5f;
+    [Header("slime動畫元件")]
+    public Animator Anislime;
+    [Header("TurtleShell動畫元件")]
+    public Animator AniTurtleShell;
+    public float size = 0.3f;
 
     private void Start()
     {
@@ -23,5 +28,24 @@ public class gamemanager : MonoBehaviour
 
         slime.Rotate(0, joystick.Horizontal, 0);
         TurtleShell.Rotate(0, joystick.Horizontal * turn , 0);
+
+
+        slime.localScale += new Vector3(1, 1, 1) * joystick.Vertical * size;
+    }
+
+    public void Walk()
+    {
+        print("走路");
+        Anislime.SetTrigger("走路觸發");
+    }
+    public void Run()
+    {
+        print("疾走");
+        Anislime.SetTrigger("疾走觸發");
+    }
+    public void Dizzy()
+    {
+        print("暈眩");
+        Anislime.SetTrigger("暈眩觸發");
     }
 }
